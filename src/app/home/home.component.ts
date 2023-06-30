924787118bc3b0b910a15ec6829732d8453a721a
Sabin Subedi <sabinsubedi125@gmail.com>
2023-06-30 20:07:36 +0545

    binding data and interpolation in angular

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-home',
@@ -14,9 +15,23 @@ import { HousingLocationComponent } from '../housing-location/housing-location.c
       </form>
     </section>
     <section>
-      <app-housing-location></app-housing-location>
+      <app-housing-location
+        [housingLocation]="housingLocation"
+      ></app-housing-location>
     </section>
   `,
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  housingLocation: HousingLocation = {
+    id: 1,
+    name: 'The Bay Club',
+    city: 'San Francisco',
+    state: 'CA',
+    photo:
+      'https://images.unsplash.com/photo-1568605114967-8130f3a36994?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8aG91c2V8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=1000&h=1000&q=100',
+    availableUnits: 3,
+    wifi: true,
+    laundry: true,
+  };
+}
