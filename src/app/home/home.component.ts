9be47653e99cace39828b8aab10aeb6b901dc4e5
Sabin Subedi <sabinsubedi125@gmail.com>
2023-06-30 19:52:35 +0545

    add the housing location

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
-  template: ` <p>home works!</p> `,
-  styleUrls: ['./home.component.scss'],
+  imports: [CommonModule, HousingLocationComponent],
+  template: `
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city" />
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
+    <section>
+      <app-housing-location></app-housing-location>
+    </section>
+  `,
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {}
